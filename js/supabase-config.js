3b4e56dac6a4afcbdb22bb5e986550a096595ba5
Bryan Rory <bryanrory13@gmail.com>
2026-03-13 23:35:26 -0300

    fix: corrigir conflito de declaração do client Supabase

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -5,4 +5,4 @@
 const SUPABASE_URL = 'https://utvlgluyqloxnymejqml.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0dmxnbHV5cWxveG55bWVqcW1sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0NTQ2MDQsImV4cCI6MjA4OTAzMDYwNH0.HfwlT3gV8WjbTnFc4BgrppXY6fIFQxuAE8pH_x-T2uM';
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+var supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
